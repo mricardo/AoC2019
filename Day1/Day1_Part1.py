c680b7dee52e0a9288da726165a32f7fe8318320
Joana Gorgulho <miguelcricardo@gmail.com>
2019-12-01 09:39:13 +0000

    Day1 Update

--- Day1/Day1_Part1.py
+++ Day1/Day1_Part1.py
@@ -2,9 +2,9 @@ import sys
 import math
 
 if __name__ == "__main__":
-    sum = 0
+    total = 0
     with open(sys.argv[1]) as lines:
         for line in lines:
             number = int(line)
-            sum += math.floor((number / 3)) - 2
-    print(sum)
+            total += math.floor((number / 3)) - 2
+    print(total)
